a823385ae36b2593a96f57990663e6955e8cb272
Cohen <dcoco91@gmail.com>
2019-07-31 14:39:44 -0400

    the letter constructor has been completed, and the word constructor is almost finished. working on the check to see if the letter has been guessed correctly

--- letter.js
+++ letter.js
@@ -1,30 +1,30 @@
-var Letter = function() {
+var Letter = function(character) {
     this.character = character;
     this.isGuessed = false;
 
     this.correctCheck = function(guess) {
-
+        console.log(`\nYour Guess ====> ${guess}`);
         if (guess === this.character) {
-            console.log("that's correct!")
+            console.log("is CORRECT!");
             this.isGuessed = true;
+        } else {
+            console.log("is WRONG!");
         }
     }
 
 }
 
 Letter.prototype.toString = function() {
+    var lete = "_"
     if (this.isGuessed) {
         return this.character;
     } else {
-        return "_";
+        return lete;
     }
 }
 
 var l = new Letter;
 l.character = "e";
 
-Letter.correctCheck("d");
-console.log("-------");
-Letter.correctCheck("e");
 
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,8 +1,43 @@
 var Letter = require("./letter.js");
+var randomWords = require('random-words');
+
 
 
 var Word = function() {
+    var letters = [];
+    // getting a random word from the node module random word
+    var word = randomWords();
+    this.word = word;
+    //splitting it with a space into an array
+    var sp = word.split("");
+
+    // for each letter (prob should be a foreach function) we call the letter constructor 
+    // on it and then push that value into the letters array
+    for (var i = 0; i < sp.length; i++) {
+        var x = new Letter(sp[i]);
+        letters.push(x);
+    }
+
+    this.letters = letters;
+
+    this.showLetters = function() {
+
+        // this is an array to hold the word we're going to show the user. Depending on whether
+        // or not the letter has been guessed, we either push a "_", or the the actual letter
+        var theWord = [];
+        for (var i = 0; i < letters.length; i++) {
+
+            // pushing the letter to the array, the tostring checks if the the letter has
+            // been guessed or not
+            theWord.push(this.letters[i].toString());
+
+        }
+        return theWord.join(" ");
+    }
 
 }
+
+var g = new Word;
+g.letters[0].isGuessed = true;
 
 module.exports = Word;
