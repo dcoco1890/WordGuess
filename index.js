593bb2deef867448946b79ee4725c10b98d9fc57
herman2 <herman2@herman2s-MBP.fios-router.home>
2019-08-02 13:16:57 -0400

    worked on a little, index now should bring back word with _ for letters that are not guessed

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var inquirer = require("inquirer");
 var Word = require("./word.js");
+var ui = new inquirer.ui.BottomBar();
 
 
 function playGame() {
 
     var cWord = new Word;
-    cWord.toString();
+    let chars = [];
 
-    // inquirer.prompt([
-    //     {
-    //         type
-    //     }
-    // ])
+    for (var i = 0; i < cWord.letters.length; i++) {
+
+        chars.push(cWord.letters[i].character);
+    }
+    // cWord.showLetters();
+    // cWord.letters[0].isGuessed = true;
+    cWord.showLetters();
+
+    ui.log.write('enter ONLY a single character letter ("a" OR "b" etc..)');
+    inquirer.prompt([{
+        name: "userInput",
+        type: "input",
+        message: "Make a guess!",
+    }])
 }
-
+console.log("Welcome to Word Guess!\n----------------------------------\n");
 playGame();
--- word.js
+++ word.js
@@ -15,7 +15,7 @@ var Word = function() {
     // on it and then push that value into the letters array
     for (var i = 0; i < sp.length; i++) {
         var x = new Letter(sp[i]);
-        letters.push(x.toString());
+        letters.push(x);
     }
 
     this.letters = letters;
@@ -24,20 +24,21 @@ var Word = function() {
 
         // this is an array to hold the word we're going to show the user. Depending on whether
         // or not the letter has been guessed, we either push a "_", or the the actual letter
+        for (var i = 0; i < letters.length; i++) {
+            if (this.letters[i].isGuessed === true) {
+
+            }
+        }
         var theWord = this.letters.join(" ");
         console.log(theWord);
-
-
     }
 
-    // this.checker = function (){
 
-    // }
 
 }
 
-var g = new Word;
-g.letters[0].isGuessed = true;
-g.correctCheck("e");
-g.showLetters();
+// var g = new Word;
+// g.letters[0].isGuessed = true;
+// console.log(g.letters[0].character);
+// g.showLetters();
 module.exports = Word;
